207c6d7a12d7385b7d3910b2946debc502c67549
Sherniyaz Zhakenov <sherniyazz@gmail.com>
2021-10-10 23:25:53 +0600

    first

---
+++ word-hunt/src/Components/Definitions/Definitions.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import './Definitions.css';
+
+const Definitions = ({word, meanings, category, LightMode}) => {
+    return (<div className="meanings">
+        {
+            meanings[0] && word && category=== 'en' && (
+                <audio
+                 src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio}
+                  style={{ backgroundColor: '#fff', borderRadius: 50, width: "100%"}}
+                  controls
+                  >
+                Your browser does not support audio.
+                </audio>
+            )
+        }
+        {word ===""?(
+        <span className='subTitle'>
+            Start by typing a word in Search
+        </span>) :  (
+                meanings.map((mean) => (
+                    mean.meanings.map((item) => (
+                         item.definitions.map((def) => (
+                         <div
+                          className = "singleMean"
+                           style={{backgroundColor:LightMode ? "#3b5360" : "white", color:LightMode ? "white" : "black"}}
+                           >  
+                        <b>{def.definition}</b>
+                        <hr style={{backgroundcolor: "black", width:"100%"}}/>
+                        {
+                            def.example && (<span>
+                                <b>Example: </b>
+                                {def.example}
+                            </span>)
+                        }
+                        {
+                            def.synonyms && (<span>
+                                <b>Synonyms: </b>
+                                {def.synonyms.map((s) => `${s}, `)}
+                                </span>)}
+                         </div>
+                         ))
+                         ))
+                    ))
+                    )}
+            </div>
+    ); 
+};
+
+export default Definitions
---
+++ word-hunt/src/Components/Header/Header.js
@@ -0,0 +1,50 @@
+import {createTheme, MenuItem, TextField, ThemeProvider } from '@material-ui/core';
+import React from 'react';
+import './Header.css';
+import categories from '../../data/category';
+const Header = ({setCategory, category, setWord, word, LightMode}) => {
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main:LightMode ? "#000" :'#fff',
+            },
+            type: LightMode ? "light" : 'dark',
+        },
+    });
+    const handleChange = (language) => {
+    setCategory(language);
+    setWord("");
+    };
+    return (
+        <div className='header'>
+            <span className="title">{word ? word: "Word Hunt"}</span>
+            <div className='inputs'>
+                <ThemeProvider theme={darkTheme}>
+                    <TextField
+                        className="search"
+                        label="Search a Word"
+                        id="standard-basic"
+                        value={word}
+                        onChange={(e) => setWord(e.target.value)}
+                        variant="standard"
+                    />
+                    <TextField
+                        className="select"
+                        select
+                        label="Language"
+                        value={category}
+                        onChange={(e) => handleChange(e.target.value)}
+                    >
+                        {
+                            categories.map((option) => (
+                                <MenuItem key={option.label} value={option.label} >{option.value}</MenuItem>
+                                ))}
+                                                
+                    </TextField>
+                </ThemeProvider>
+            </div>
+        </div>
+    )
+}
+
+export default Header
